1badbc8daaa84d253f55b29f1575b2223aad086d
jjbenson <jim.benson@live.co.uk>
2015-03-08 22:28:16 +0000

    Added default template directory with default error file.

--- config/server-config.js
+++ config/server-config.js
@@ -1,12 +1,12 @@
 var thymolServerConfiguration = {
-  host: "localhost",                                                          // Server host defaults to 0.0.0.0
-  port: 3000,                                                                 // Server listening port defaults to 3000
-  templateRoot: __dirname + "/../../thymol/target/test-classes/templates",    // Path to templates root directory
+  host: "localhost",                      // Server host defaults to 0.0.0.0
+  port: 3000,                             // Server listening port defaults to 3000
+  templateRoot: __dirname + "/default",   // Path to templates root directory
   templatePath : "",
   debug : true,
-/*  jQueryConfiguration: {                                                    // Properties to be copied to jQuery
+/*  jQueryConfiguration: {                  // Properties to be copied to jQuery
     support: {
-      cors: true                                                              // Required if you need to use jquery@1*
+      cors: true                          // Required if you need to use jquery@1*
     }
   },*/
   defaults: {
