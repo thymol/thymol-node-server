94e982572f057e4d89dd92abb0e1c8df12aed1ab
jjbenson <jim.benson@live.co.uk>
2015-03-08 21:32:57 +0000

    Add section for configuring jQuery. Tidy up.

--- config/server-config.js
+++ config/server-config.js
@@ -3,9 +3,13 @@ var thymolServerConfiguration = {
   port: 3000,                                                                 // Server listening port defaults to 3000
   templateRoot: __dirname + "/../../thymol/target/test-classes/templates",    // Path to templates root directory
   templatePath : "",
-  templateOffset: "",
   debug : true,
-  defaults: {      
+/*  jQueryConfiguration: {                                                    // Properties to be copied to jQuery
+    support: {
+      cors: true                                                              // Required if you need to use jquery@1*
+    }
+  },*/
+  defaults: {
     prefix : "th",
     dataPrefix : "data",
     precision : 10,
@@ -18,7 +22,7 @@ var thymolServerConfiguration = {
     extendedMapping: false,
     localMessages: true,
     disableMessages: false,
-    templateSuffix: ".html"               
+    templateSuffix: ".html"
   }
 };
   
