3af8c9c9c541666c821ff6acf64c31d0ec34d6ab
jjbenson <jim.benson@live.co.uk>
2015-03-22 23:03:14 +0000

    Refined controls.

--- config/server-config.js
+++ config/server-config.js
@@ -1,7 +1,7 @@
 var thymolServerConfiguration = {
   host: "localhost",                      // Server host defaults to 0.0.0.0
   port: 3000,                             // Server listening port defaults to 3000
-  templateRoot: __dirname + "/../default",   // Path to templates root directory
+  webappRoot: __dirname + "/../default",   // Path to templates root directory
   templatePath : "",
   debug : true,
 /*  jQueryConfiguration: {                  // Properties to be copied to jQuery
@@ -17,6 +17,7 @@ var thymolServerConfiguration = {
     locale : "en",
     precedence : 20000,
     messagePath : "",
+    resourcePath : "",
     messagesBaseName : "Messages",
     relativeRootPath: "",
     extendedMapping: false,
